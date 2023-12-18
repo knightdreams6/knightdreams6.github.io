df84b43b9fe09d4e7bf8660f442ac4462baffd04
lixiao <knightdreams6@163.com>
2023-12-18 16:28:51 +0800

    :memo: 个人简历模块调整

--- .vuepress/components/resume.vue
+++ .vuepress/components/resume.vue
@@ -2,41 +2,69 @@
   <div>
     <a :href="resumePdf" target="_blank">点击查看附件简历</a>
     <h3>以下为简历中提到做过的部分项目/模块示例：</h3>
-    <el-alert title="Tips: 点击图片查看更多" type="info"/>
     <div>
       <h5>消息推送模块</h5>
-      <el-image :hide-on-click-modal="true" :src="messagePushImgSrcList[0]" :preview-src-list="messagePushImgSrcList"
-                :infinite="false"/>
+      <el-carousel :autoplay="false" height="460px" arrow="always">
+        <el-carousel-item v-for="item in messagePushImgSrcList" :key="item">
+          <el-image :hide-on-click-modal="true" :src="item"
+                    :infinite="false"/>
+        </el-carousel-item>
+      </el-carousel>
     </div>
     <div>
       <h5>风险认证模块</h5>
-      <el-image :hide-on-click-modal="true" :src="apiRiskImgSrcList[0]" :preview-src-list="apiRiskImgSrcList"
-                :infinite="false"/>
+      <el-carousel :autoplay="false" height="520px" arrow="always">
+        <el-carousel-item v-for="item in apiRiskImgSrcList" :key="item">
+          <el-image :hide-on-click-modal="true" :src="item"
+                    :infinite="false"/>
+        </el-carousel-item>
+      </el-carousel>
     </div>
     <div>
       <h5>存储模块</h5>
-      <el-image :hide-on-click-modal="true" :src="storageImgSrcList[0]" :preview-src-list="storageImgSrcList"
-                :infinite="false"/>
+      <el-carousel :autoplay="false" height="520px" arrow="always">
+        <el-carousel-item v-for="item in storageImgSrcList" :key="item">
+          <el-image :hide-on-click-modal="true" :src="item"
+                    :infinite="false"/>
+        </el-carousel-item>
+      </el-carousel>
     </div>
     <div>
       <h5>TMS工作站</h5>
-      <el-image :hide-on-click-modal="true" :src="tmsStationImgSrcList[0]" :preview-src-list="tmsStationImgSrcList"
-                :infinite="false"/>
+      <el-carousel :autoplay="false" height="520px" arrow="always">
+        <el-carousel-item v-for="item in tmsStationImgSrcList" :key="item">
+          <el-image :hide-on-click-modal="true" :src="item"
+                    :infinite="false"/>
+        </el-carousel-item>
+      </el-carousel>
     </div>
     <div>
       <h5>TMS模拟器</h5>
-      <el-image :hide-on-click-modal="true" :src="tmsMockImgSrcList[0]" :preview-src-list="tmsMockImgSrcList"
-                :infinite="false"/>
+      <el-carousel :autoplay="false" height="580px" arrow="always">
+        <el-carousel-item v-for="item in tmsMockImgSrcList" :key="item"
+                          style="display: flex; justify-content: center; align-items: center">
+          <el-image :hide-on-click-modal="true" :src="item"
+                    :infinite="false"/>
+        </el-carousel-item>
+      </el-carousel>
     </div>
     <div>
       <h5>启动器</h5>
-      <el-image :hide-on-click-modal="true" :src="launcherImgSrcList[0]" :preview-src-list="launcherImgSrcList"
-                :infinite="false"/>
+      <el-carousel :autoplay="false" height="520px" arrow="always">
+        <el-carousel-item v-for="item in launcherImgSrcList" :key="item">
+          <el-image :hide-on-click-modal="true" :src="item"
+                    :infinite="false"/>
+        </el-carousel-item>
+      </el-carousel>
     </div>
     <div>
       <h5>个人开源项目</h5>
-      <el-image :hide-on-click-modal="true" :src="openImgSrcList[0]" :preview-src-list="openImgSrcList"
-                :infinite="false"/>
+      <el-carousel :autoplay="false" height="500px" arrow="always">
+        <el-carousel-item v-for="item in openImgSrcList" :key="item">
+          <el-image :hide-on-click-modal="true" :src="item"
+                    :infinite="false"/>
+        </el-carousel-item>
+      </el-carousel>
     </div>
     
     <div>
@@ -91,7 +119,7 @@ onMounted(() => {
   const launcherImg = ['launcher1.jpeg', 'launcher2.jpeg']
   launcherImgSrcList.value = launcherImg.map(src => hostname.value + src)
   
-  const openImg = ['open1.jpeg', 'open2.jpeg', 'open3.jpeg', 'open4.jpeg', 'open5.jpeg', 'open6.jpeg', 'open7.jpeg']
+  const openImg = ['open.jpeg', 'open1.jpeg', 'open2.jpeg', 'open3.jpeg', 'open4.jpeg', 'open5.jpeg', 'open6.jpeg', 'open7.jpeg', 'open8.jpeg']
   openImgSrcList.value = openImg.map(src => hostname.value + src)
   
   wxImgSrc.value = hostname.value + 'wx.jpeg'
